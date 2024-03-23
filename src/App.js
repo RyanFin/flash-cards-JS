a42696025c61e9516df4538fd00cd6a88b1ef0cd
RyanFin <ryanfinlayson125@hotmail.com>
2024-03-23 20:08:40 +0000

    add code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
@@ -12,40 +13,56 @@ const questions = [
   {
     id: 3457,
     question: "What language is React based on?",
-    answer: "JavaScript"
+    answer: "JavaScript",
   },
   {
     id: 7336,
     question: "What are the building blocks of React apps?",
-    answer: "Components"
+    answer: "Components",
   },
   {
     id: 8832,
     question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
+    answer: "JSX",
   },
   {
     id: 1297,
     question: "How to pass data from parent to child components?",
-    answer: "Props"
+    answer: "Props",
   },
   {
     id: 9103,
     question: "How to give components memory?",
-    answer: "useState hook"
+    answer: "useState hook",
   },
   {
     id: 2002,
     question:
       "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
+    answer: "Controlled element",
+  },
 ];
 
 function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null); //no item to be selected in the beginning
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
   return (
     <div className="flashcards">
-        {questions.map(question => <div key={question.id}><p>{question.question}</p></div> )}
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)} //react can call the function when it is ready with () =>
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
